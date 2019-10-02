019f451dfecce598715f81636d853598d983447f
Vadim <chakapega@gmail.com>
2019-10-02 22:04:40 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+  const arrayOfNumbers = expression.split('*');
+  let numberOfTwos = 0;
+  let numberOfFives = 0;
+
+  const calculateTwosAndFivesInFactorial = number => {
+    for (let i = number; i > 0; i -= 1) {
+      if (i % 2 === 0) {
+        numberOfTwos++;
+      };
+      
+      if (i % 5 === 0) {
+        numberOfFives++;
+      };
+
+      if (i % 25 === 0) {
+        numberOfFives++;
+      };
+    };
+  };
+
+  const calculateTwosAndFivesInDoubleFactorial = number => {
+    for (let i = number; i > 0; i -= 2) {
+      if (i % 2 === 0) {
+        numberOfTwos++;
+      };
+      
+      if (i % 5 === 0) {
+        numberOfFives++;
+      };
+
+      if (i % 25 === 0) {
+        numberOfFives++;
+      };
+    };
+  };
+
+  arrayOfNumbers.forEach(n => {
+    if (n[n.length - 1] === '!' && n[n.length - 2] !== '!') {
+      const number = parseInt(n);
+
+      calculateTwosAndFivesInFactorial(number);
+    };
+
+    if (n[n.length - 1] === '!' && n[n.length - 2] === '!') {
+      const number = parseInt(n);
+
+      calculateTwosAndFivesInDoubleFactorial(number);
+    };
+  });
+
+  return Math.min(numberOfTwos, numberOfFives);
+};
